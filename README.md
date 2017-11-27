a8d8428970aa230c8bab5046c548905cf5f6369f
David Henley <henleydr@gmail.com>
2017-11-26 20:01:25 -0600

    Create Table

---
+++ README.md
@@ -0,0 +1,17 @@
+AWS SDK with DynamoDB
+=====================
+
+Usage
+-----
+
+**Install**
+
+`npm install`
+
+**Add Amazon AWS Keys**
+
+Modify `src/config/aws-keys.json` using example file.
+
+**Create A Table**
+
+`npm run create`
---
+++ src/ddb_createtable.js
@@ -0,0 +1,42 @@
+const AWS = require('aws-sdk');
+const path = require('path');
+
+AWS.config.loadFromPath(path.join(__dirname, 'config', 'aws-keys.json'));
+
+const ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+
+const params = {
+  AttributeDefinitions: [
+    {
+      AttributeName: "CUSTOMER_ID",
+      AttributeType: "N"
+    },
+    {
+      AttributeName: "CUSTOMER_NAME",
+      AttributeType: "S"
+    }
+  ],
+  KeySchema: [
+    {
+      AttributeName: "CUSTOMER_ID",
+      KeyType: "HASH"
+    },
+    {
+      AttributeName: "CUSTOMER_NAME",
+      KeyType: "RANGE"
+    }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 1,
+    WriteCapacityUnits: 1
+  },
+  TableName: "CUSTOMER_LIST3"
+};
+
+ddb.createTable(params, function(err, data) {
+  if (err) {
+    console.log('Error', err.message);
+  } else {
+    console.log('Success', JSON.stringify(data.TableDescription.KeySchema, null, 2));
+  }
+});
